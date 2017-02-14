17dc78cd805d4d5e295ce0b8a0f259a5e131c015
arpit-saxena <arpit.saxena2000@yahoo.in>
2017-02-14 19:24:59 +0530

    Fixed error that was to be fixed earlier

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <conio.h>
 #include "quiz.h"
 
+extern question questions[3][3][3];
+
 void main()
 {
 	clrscr();
-	
+
 	init_ques();
 	init_ui();
-	
-	cout << questions[0][1][2].q;
-	
+
 	getch();	
 }
