8e6e4b6c270fb8b2d08de0094cdab456e1d8e9a7
arpit-saxena <arpit.saxena2000@yahoo.in>
2017-02-15 17:07:01 +0530

    Adding features in progress... Saving progress to restore in case of mishaps...

--- ui.cpp
+++ ui.cpp
@@ -2,6 +2,9 @@
 #include <conio.h>
 #include <iostream.h>
 #include "quiz.h"
+#include <stdio.h>
+
+#define TITLE "Some Great Awesome Title Here"
 
 extern question questions[3][3][3];
 
@@ -15,29 +18,6 @@ void init_ui()
 	width = (int) info.screenwidth;
 	height = (int) info.screenheight;
 }
-
-/*void printq(int a, int b, int c, int q_no)
-{
-	struct text_info info;
-	gettextinfo(&info);
-
-	int width = (int) info.screenwidth;
-	int height = (int) info.screenheight;
-
-	int text_w = (9 * width) / 10; //Defines the minimum whitespace on both sides when printing the question
-	int text_h = strlen(questions[a][b][c].q) / text_w;
-
-	for(int i = 0; i < 4; i++)
-	{
-		text_h += strlen(questions[a][b][c].options[i]) / text_w;
-	}
-
-	text_h += 1;
-
-	gotoxy(1, (height - text_h) / 2);
-
-
-} */
 
 void frame(coord up_left, int f_height, int f_width, int f_sides)
 {
@@ -107,20 +87,19 @@ void printc(char str[], int ws, int t_color, int b_color)
 	1: difficulty level
 	2: Subject
 */
-int generate_ui(int screen_num, int lvl, int sub, int q_num)
+int generate_ui(int screen_num)
 {
 	clrscr();
 	frame();
 
-	char title[] = "SOME GREAT AWESOME TITLE HERE";
-	char head[100], op1[100], op2[100], op3[100], op4[100];
+	char title[] = TITLE;
+	char head[100], op1[100], op2[100], op3[100];
 	if(screen_num == 1)
 	{
 		strcpy(head, "Select difficulty level: ");
 		strcpy(op1, "Easy");
 		strcpy(op2, "Intermediate");
 		strcpy(op3, "Hard");
-		strcpy(op4, "");
 	}
 	else if(screen_num == 2)
 	{
@@ -128,23 +107,9 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 		strcpy(op1, "Computer Science (C++)");
 		strcpy(op2, "General Knowledge");
 		strcpy(op3, "English");
-		strcpy(op4, "");
-	}
-	else if(screen_num == 3)
-	{
-		strcpy(head, questions[lvl][sub][q_num].q);
-		strcpy(op1, questions[lvl][sub][q_num].options[0]);
-		strcpy(op2, questions[lvl][sub][q_num].options[1]);
-		strcpy(op3, questions[lvl][sub][q_num].options[2]);
-		strcpy(op4, questions[lvl][sub][q_num].options[3]);
 	}
 
 	gotoxy(1, (height - 6) / 2);
-
-	if(screen_num == 3)
-	{
-		gotoxy(1, (height - 7) / 2);
-	}
 
 	printc(title);
 	cout << "\n\n";
@@ -154,14 +119,13 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 					  strlen(op1),
 					  strlen(op2),
 					  strlen(op3),
-					  strlen(op4)
 					};
 
 	//Determining max length
 	int max_len = lengths[0];
-	for(int i = 0; i < 5; i++)
+	for(int i = 0; i < 4; i++)
 	{
-		for(int j = i + 1; j < 5; j++)
+		for(int j = i + 1; j < 4; j++)
 		{
 			if(lengths[j] > max_len)
 			{
@@ -172,7 +136,7 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 
 	int a = (width - max_len) / 2;
 	coord b(a - 4,wherey());
-	frame(b, (screen_num == 3 ? 7 : 6), max_len + 8, 0);
+	frame(b, 6, max_len + 8, 0);
 
 	printc(head, a);
 	cout << '\n';
@@ -186,23 +150,10 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 		printc(op1, a); cout << '\n';
 		printc(op2, a); cout << '\n';
 		printc(op3, a); cout << '\n';
-		if(screen_num == 3)
-		{
-			printc(op4, a); cout << '\n';
-		}
-
-		int num;
-		if(screen_num == 3)
-		{
-			num = 4;
-		}
-		else
-		{
-			num = 3;
-		}
 
 		//Highlighting the option to show current line
-		for(int i = 0; i < num; i++)
+		int num_op = 3;            //Number of options
+		for(int i = 0; i < num_op; i++)
 		{
 			if(i == curr_line)
 			{
@@ -215,6 +166,7 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 				cout << ' ';
 			}
 		}
+
 		//Detecting arrow keys
 		char c = getch();
 		if(c == 0)
@@ -229,10 +181,10 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 					curr_line++;
 					break;
 			}
-			curr_line %= num;
+			curr_line %= num_op;
 			if(curr_line < 0)
 			{
-				curr_line += num;
+				curr_line += num_op;
 			}
 		}
 		else if(c == 13) //If enter was pressed
@@ -242,4 +194,138 @@ int generate_ui(int screen_num, int lvl, int sub, int q_num)
 	}
 
 	return curr_line; //The line that was selected
+}
+
+int generate_ui(int lvl, int sub, int q_num)
+{
+	clrscr();
+	frame();
+
+	char ques[200], options[4][200];
+
+	strcpy(ques, questions[lvl][sub][q_num].q);
+	strcpy(options[0], questions[lvl][sub][q_num].options[0]);
+	strcpy(options[1], questions[lvl][sub][q_num].options[1]);
+	strcpy(options[2], questions[lvl][sub][q_num].options[2]);
+	strcpy(options[3], questions[lvl][sub][q_num].options[3]);
+
+	int f_width = (8 * width) / 10;
+	int ws = (width - f_width) / 2;
+
+	int lengths[] = {
+					 strlen(ques),
+					 strlen(options[0]),
+					 strlen(options[1]),
+					 strlen(options[2]),
+					 strlen(options[3])
+					};
+
+	int f_height = 2 + lengths[0] / f_width + 1;
+	int f_ws = 3;  //Whitespace inside frame for options
+	for(int i = 1; i < 5; i++)
+	{
+		f_height += lengths[i] / (f_width - f_ws) + 1;
+	}
+
+	coord u_left(ws + 1, (height - f_height) / 2 + 1);
+	frame(u_left, f_height, f_width, 0);
+
+	char fstring[200] = "";
+
+
+
+	wrap(ques, fstring, f_width - f_ws);
+
+	int line_num = 1;
+	char line[200];
+	int read;
+	int chars_read = 0;
+	while(sscanf(fstring + chars_read, "%[^\n]%*c%n", line, &read) > 0)
+	{
+		gotoxy(u_left.x + 2, u_left.y + line_num + 1);
+		cout << line;
+		line_num++;
+		chars_read += read;
+		if(chars_read >= strlen(fstring))
+		{
+			break;
+		}
+	}
+
+	strcpy(fstring, "");
+
+	int selected = 0; //Boolean value to express whether any option has been chosen
+	int curr_line = 0; //Indicates the position of current line that is highlighted
+	while(!selected)
+	{
+		for(i = 0; i < 4; i++)
+		{
+			wrap(options[i], fstring, f_width - f_ws - 3 - 2);
+			read = 0, chars_read = 0;
+
+			gotoxy(u_left.x + 2 + 3, u_left.y + line_num + 1);
+			cout << (char) ('A' + i) << ". ";
+
+			int len = strlen(fstring);
+			fstring[len] = '\n'; fstring[len+1] = '\0';
+
+			while(sscanf(fstring + chars_read, "%[^\n]%*c%n", line, &read) > 0)
+			{
+				gotoxy(u_left.x + 2 + 3 + 3, u_left.y + line_num + 1);
+				cout << line;
+				line_num++;
+				chars_read += read;
+				if(chars_read >= strlen(fstring))
+				{
+					break;
+				}
+			}
+
+			strcpy(fstring, "");
+		}
+		
+		//Highlighting the option to show current line
+		int num_op = 4;            //Number of options
+		for(int i = 0; i < num_op; i++)
+		{
+			if(i == curr_line)
+			{
+				gotoxy(u_left.x + 2, u_left.y + 2 + lengths[0] + i);
+				cout << (char) 175;
+			}
+			else
+			{
+				gotoxy(u_left.x + 2, u_left.y + 2 + lengths[0] + i);
+				cout << ' ';
+			}
+		}
+
+		//Detecting arrow keys
+		char c = getch();
+		if(c == 0)
+		{
+			c = getch();
+			switch(c)
+			{
+				case 'H':
+					curr_line--;
+					break;
+				case 'P':
+					curr_line++;
+					break;
+			}
+			curr_line %= num_op;
+			if(curr_line < 0)
+			{
+				curr_line += num_op;
+			}
+		}
+		else if(c == 13) //If enter was pressed
+		{
+			return curr_line;
+		}
+	 }
+
+
+	return 0;
 }
